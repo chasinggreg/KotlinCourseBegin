4d8e5789cfacc712eb33a0e25c70362737b14579
Chasinggreg <gregsmith@Gregs-MBP.attlocal.net>
2023-09-30 14:40:05 -0500

    Initial

---
+++ FloatAndDouble/src/main/kotlin/Main.kt
@@ -0,0 +1,4 @@
+fun main(args: Array<String>) {
+    val myNumber = 2.5f
+    println(myNumber)
+}
---
+++ Variables/src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var userName: = "Greg"
+    userName = "John"
+    var age: Int = 22
+    age = 25
+    println("Hello $userName your age is $age")
+
+}
